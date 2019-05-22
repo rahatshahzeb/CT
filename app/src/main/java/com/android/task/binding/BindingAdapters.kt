e31044b1a0f8f381fcf4233e7a9c86df49f2cf5a
Shahzeb Rahat <rahat.shahzeb@gmail.com>
2019-05-22 10:35:58 +0800

    Error label animation added

--- app/src/main/java/com/android/task/binding/BindingAdapters.kt
+++ app/src/main/java/com/android/task/binding/BindingAdapters.kt
@@ -6,23 +6,23 @@ import androidx.databinding.BindingAdapter
 object BindingAdapters {
 
     /**
-     * A Binding Adapter that is called whenever the value of the attribute `visibleInvisible`
-     * changes.
-     */
-    @JvmStatic
-    @BindingAdapter("visibleInvisible")
-    fun setInvisible(view: View, show: Boolean) {
-        view.visibility = if (show) View.VISIBLE else View.INVISIBLE
-    }
-
-    /**
      * A Binding Adapter that is called whenever the value of the attribute `visibleGone`
      * changes.
      */
     @JvmStatic
     @BindingAdapter("visibleGone")
     fun setGone(view: View, show: Boolean) {
-        view.visibility = if (show) View.VISIBLE else View.GONE
+        view.visibility = if (show) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+
+        if (show) {
+            view.animate().alpha(1.0f)
+        } else {
+            view.animate().alpha(0.0f)
+        }
     }
 
 }
